f3a89e60da79bca3c2284f4271a4829a5b8b125c
Freeman136 <a13661777@gmail.com>
2024-02-18 20:28:43 +0300

    Initial commit

---
+++ UnsplashImage/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  UnsplashImage
+//
+//  Created by Andrew on 18.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
